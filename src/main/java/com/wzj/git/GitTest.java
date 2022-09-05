41f52e2c6dac3113808b6351282d766337aa8cf1
paomo666 <65238884+paomo666@users.noreply.github.com>
2022-09-05 23:30:09 +0800

    pull test

--- src/main/java/com/wzj/git/GitTest.java
+++ src/main/java/com/wzj/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hot-test!!!");
         System.out.println("hot-test!!!");
         System.out.println("push test!!!");
+        System.out.println("pull test!!!");
     }
 }
