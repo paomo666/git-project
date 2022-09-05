57a009bb53105ac2615d54966188f316724f7701
wzj <2572551708@qq.com>
2022-09-05 22:35:43 +0800

    second commit

---
+++ src/main/java/com/wzj/git/GitTest2.java
@@ -0,0 +1,13 @@
+package com.wzj.git;
+
+/**
+ * @author ：wzj
+ * @date ：Created in 2022/9/5 22:33
+ * @description：
+ * @version: 1.0
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("get test02!");
+    }
+}
