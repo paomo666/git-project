7d8f7f8674d215e3fc8eac40bf976e68756c18b8
wzj <2572551708@qq.com>
2022-09-05 22:49:23 +0800

    hot-fox commit1

--- src/main/java/com/wzj/git/GitTest.java
+++ src/main/java/com/wzj/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("hello world!");
+        System.out.println("hot-fix");
     }
 }
