e38f6a0f019f0c862b63ae9d5b2d9fbc420b4669
wzj <2572551708@qq.com>
2022-09-05 22:32:26 +0800

    first commit

---
+++ src/main/java/com/wzj/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.wzj.git;
+
+/**
+ * @author ：wzj
+ * @date ：Created in 2022/9/5 22:24
+ * @description：
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+    }
+}
